53658984ab96fb25bd221769f8c079b2e47e2f97
kamal-maverick <kamalshanthdeveloper163@gmail.com>
2025-07-21 20:08:33 +0530

    Add ReportsForm

--- eShift/Forms/Shared/ReportsForm.cs
+++ eShift/Forms/Shared/ReportsForm.cs
@@ -1,18 +1,23 @@
 ﻿using eShift.Utilities;
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Drawing.Printing;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Printing;
 using System.Windows.Forms;
 
 namespace eShift.Forms.Shared
 {
     public partial class ReportsForm : Form
     {
+        private DataGridView dgvReport;
+        private Button btnPrint;
+        private Button btnClose;
+        private Button btnGenerate;
+        private Button btnViewLoads;
+        private Label label1;
+        private ComboBox cmbReportType;
+        private Label lblTitle;
+
         public ReportsForm()
         {
             InitializeComponent();
@@ -114,6 +119,117 @@ namespace eShift.Forms.Shared
                     yPos += font.GetHeight() + 10;
                 }
             }
+        }
+
+        private void InitializeComponent()
+        {
+            this.dgvReport = new System.Windows.Forms.DataGridView();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnViewLoads = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbReportType = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReport)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvReport
+            // 
+            this.dgvReport.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvReport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReport.Location = new System.Drawing.Point(98, 168);
+            this.dgvReport.Name = "dgvReport";
+            this.dgvReport.ReadOnly = true;
+            this.dgvReport.Size = new System.Drawing.Size(750, 246);
+            this.dgvReport.TabIndex = 42;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(696, 61);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(84, 23);
+            this.btnPrint.TabIndex = 41;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(810, 61);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 40;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(443, 61);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(90, 23);
+            this.btnGenerate.TabIndex = 39;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            // 
+            // btnViewLoads
+            // 
+            this.btnViewLoads.Location = new System.Drawing.Point(557, 61);
+            this.btnViewLoads.Name = "btnViewLoads";
+            this.btnViewLoads.Size = new System.Drawing.Size(118, 23);
+            this.btnViewLoads.TabIndex = 38;
+            this.btnViewLoads.Text = "View Loads";
+            this.btnViewLoads.UseVisualStyleBackColor = true;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(50, 58);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(174, 24);
+            this.lblTitle.TabIndex = 37;
+            this.lblTitle.Text = "Generate Reports";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 44;
+            this.label1.Text = "Report Type";
+            // 
+            // cmbReportType
+            // 
+            this.cmbReportType.FormattingEnabled = true;
+            this.cmbReportType.Items.AddRange(new object[] {
+            "[\"Customer\", \"Admin\"]"});
+            this.cmbReportType.Location = new System.Drawing.Point(205, 123);
+            this.cmbReportType.Name = "cmbReportType";
+            this.cmbReportType.Size = new System.Drawing.Size(216, 21);
+            this.cmbReportType.TabIndex = 43;
+            // 
+            // ReportsForm
+            // 
+            this.ClientSize = new System.Drawing.Size(934, 511);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbReportType);
+            this.Controls.Add(this.dgvReport);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnViewLoads);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "ReportsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReport)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
     }
 }
