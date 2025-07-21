e2150db2ebb52374e13965a666935d585c93b38f
kamal-maverick <kamalshanthdeveloper163@gmail.com>
2025-07-21 19:30:37 +0530

    Add AddProductForm

--- eShift/Forms/AdminForms/AddProductForm.cs
+++ eShift/Forms/AdminForms/AddProductForm.cs
@@ -1,16 +1,21 @@
 ﻿using eShift.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Linq;
 
 namespace eShift.Forms.AdminForms
 {
     public partial class AddProductForm : Form
     {
+        private Button btnCancel;
+        private Button btnSave;
+        private Label label8;
+        private Label label2;
+        private TextBox txtDescription;
+        private TextBox txtName;
+        private Label label3;
+        private TextBox txtHandlingFee;
+        private Label label1;
+
         public AddProductForm()
         {
             InitializeComponent();
@@ -60,6 +65,117 @@ namespace eShift.Forms.AdminForms
         {
             this.DialogResult = DialogResult.Cancel;
             this.Close();
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHandlingFee = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(496, 408);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 57;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(396, 408);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 56;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(279, 254);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 13);
+            this.label8.TabIndex = 55;
+            this.label8.Text = "Description";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(277, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 54;
+            this.label2.Text = "Name";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(420, 247);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(238, 20);
+            this.txtDescription.TabIndex = 53;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(420, 203);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(238, 20);
+            this.txtName.TabIndex = 52;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(392, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 24);
+            this.label1.TabIndex = 51;
+            this.label1.Text = "Add New Product";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(279, 301);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 59;
+            this.label3.Text = "Handling Fee";
+            // 
+            // txtHandlingFee
+            // 
+            this.txtHandlingFee.Location = new System.Drawing.Point(420, 294);
+            this.txtHandlingFee.Name = "txtHandlingFee";
+            this.txtHandlingFee.Size = new System.Drawing.Size(238, 20);
+            this.txtHandlingFee.TabIndex = 58;
+            // 
+            // AddProductForm
+            // 
+            this.ClientSize = new System.Drawing.Size(934, 511);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtHandlingFee);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "AddProductForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
     }
 }
