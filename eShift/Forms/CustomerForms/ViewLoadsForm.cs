6f95e9d2e827d66fb3f2ad49e55c9b1bdd093bbd
kamal-maverick <kamalshanthdeveloper163@gmail.com>
2025-07-21 18:58:20 +0530

    Add ViewLoadsForm

--- eShift/Forms/CustomerForms/ViewLoadsForm.cs
+++ eShift/Forms/CustomerForms/ViewLoadsForm.cs
@@ -1,16 +1,20 @@
-﻿using eShift.Models;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace eShift.Forms.CustomerForms
 {
     public partial class ViewLoadsForm : Form
     {
+        private DataGridView dgvLoads;
+        private Button btnClose;
+        private Label lblTitle;
+        private Label lblJobNumber;
+        private DataGridViewTextBoxColumn LoadNum;
+        private DataGridViewTextBoxColumn Product;
+        private DataGridViewTextBoxColumn Qty;
+        private DataGridViewTextBoxColumn Weight;
+        private DataGridViewTextBoxColumn Instructions;
         private string jobNumber;
 
         public ViewLoadsForm(string jobNumber)
@@ -23,7 +27,7 @@ namespace eShift.Forms.CustomerForms
 
         private void LoadLoads()
         {
-            DataTable dt = Load.GetLoadsByJob(jobNumber);
+            DataTable dt = Models.Load.GetLoadsByJob(jobNumber);
             dgvLoads.DataSource = dt;
 
             // Hide unnecessary columns
@@ -39,6 +43,113 @@ namespace eShift.Forms.CustomerForms
         private void btnClose_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void InitializeComponent()
+        {
+            this.dgvLoads = new System.Windows.Forms.DataGridView();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblJobNumber = new System.Windows.Forms.Label();
+            this.LoadNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Product = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Qty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Weight = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Instructions = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLoads)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvLoads
+            // 
+            this.dgvLoads.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvLoads.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLoads.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.LoadNum,
+            this.Product,
+            this.Qty,
+            this.Weight,
+            this.Instructions});
+            this.dgvLoads.Location = new System.Drawing.Point(123, 190);
+            this.dgvLoads.Name = "dgvLoads";
+            this.dgvLoads.ReadOnly = true;
+            this.dgvLoads.Size = new System.Drawing.Size(718, 269);
+            this.dgvLoads.TabIndex = 46;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(766, 65);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 45;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(92, 51);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(118, 24);
+            this.lblTitle.TabIndex = 44;
+            this.lblTitle.Text = "View Loads";
+            // 
+            // lblJobNumber
+            // 
+            this.lblJobNumber.AutoSize = true;
+            this.lblJobNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJobNumber.Location = new System.Drawing.Point(122, 125);
+            this.lblJobNumber.Name = "lblJobNumber";
+            this.lblJobNumber.Size = new System.Drawing.Size(0, 24);
+            this.lblJobNumber.TabIndex = 47;
+            // 
+            // LoadNum
+            // 
+            this.LoadNum.HeaderText = "Load Number";
+            this.LoadNum.Name = "LoadNum";
+            this.LoadNum.ReadOnly = true;
+            // 
+            // Product
+            // 
+            this.Product.HeaderText = "Product";
+            this.Product.Name = "Product";
+            this.Product.ReadOnly = true;
+            // 
+            // Qty
+            // 
+            this.Qty.HeaderText = "Qty";
+            this.Qty.Name = "Qty";
+            this.Qty.ReadOnly = true;
+            // 
+            // Weight
+            // 
+            this.Weight.HeaderText = "Weight";
+            this.Weight.Name = "Weight";
+            this.Weight.ReadOnly = true;
+            // 
+            // Instructions
+            // 
+            this.Instructions.HeaderText = "Instructions";
+            this.Instructions.Name = "Instructions";
+            this.Instructions.ReadOnly = true;
+            // 
+            // ViewLoadsForm
+            // 
+            this.ClientSize = new System.Drawing.Size(934, 511);
+            this.Controls.Add(this.lblJobNumber);
+            this.Controls.Add(this.dgvLoads);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "ViewLoadsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLoads)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
     }
 }
