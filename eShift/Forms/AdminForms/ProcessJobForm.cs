f1665644ab864f4427625434e918449b496223d4
kamal-maverick <kamalshanthdeveloper163@gmail.com>
2025-07-21 19:51:58 +0530

    Add ProcessJobForm

--- eShift/Forms/AdminForms/ProcessJobForm.cs
+++ eShift/Forms/AdminForms/ProcessJobForm.cs
@@ -1,15 +1,18 @@
 ﻿using eShift.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace eShift.Forms.AdminForms
 {
     public partial class ProcessJobForm : Form
     {
+        private Button btnCancel;
+        private Button btnAccept;
+        private Button btnDecline;
+        private Label lblJobNumber;
+        private Label label6;
+        private Label lblTitle;
+        private RichTextBox txtRemarks;
         private string jobNumber;
 
         public ProcessJobForm(string jobNumber)
@@ -51,6 +54,101 @@ namespace eShift.Forms.AdminForms
         {
             this.DialogResult = DialogResult.Cancel;
             this.Close();
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnAccept = new System.Windows.Forms.Button();
+            this.btnDecline = new System.Windows.Forms.Button();
+            this.lblJobNumber = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtRemarks = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(543, 358);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 58;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnAccept
+            // 
+            this.btnAccept.BackColor = System.Drawing.Color.Lime;
+            this.btnAccept.Location = new System.Drawing.Point(343, 358);
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.Size = new System.Drawing.Size(75, 23);
+            this.btnAccept.TabIndex = 49;
+            this.btnAccept.Text = "Accept";
+            this.btnAccept.UseVisualStyleBackColor = false;
+            // 
+            // btnDecline
+            // 
+            this.btnDecline.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnDecline.Location = new System.Drawing.Point(443, 358);
+            this.btnDecline.Name = "btnDecline";
+            this.btnDecline.Size = new System.Drawing.Size(75, 23);
+            this.btnDecline.TabIndex = 48;
+            this.btnDecline.Text = "Decline";
+            this.btnDecline.UseVisualStyleBackColor = false;
+            // 
+            // lblJobNumber
+            // 
+            this.lblJobNumber.AutoSize = true;
+            this.lblJobNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJobNumber.Location = new System.Drawing.Point(456, 116);
+            this.lblJobNumber.Name = "lblJobNumber";
+            this.lblJobNumber.Size = new System.Drawing.Size(0, 24);
+            this.lblJobNumber.TabIndex = 47;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(238, 184);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 46;
+            this.label6.Text = "Remarks";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(77, 65);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(125, 24);
+            this.lblTitle.TabIndex = 43;
+            this.lblTitle.Text = "Process Job";
+            // 
+            // txtRemarks
+            // 
+            this.txtRemarks.Location = new System.Drawing.Point(241, 215);
+            this.txtRemarks.Name = "txtRemarks";
+            this.txtRemarks.Size = new System.Drawing.Size(455, 96);
+            this.txtRemarks.TabIndex = 59;
+            this.txtRemarks.Text = "";
+            // 
+            // ProcessJobForm
+            // 
+            this.ClientSize = new System.Drawing.Size(934, 511);
+            this.Controls.Add(this.txtRemarks);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAccept);
+            this.Controls.Add(this.btnDecline);
+            this.Controls.Add(this.lblJobNumber);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "ProcessJobForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
     }
 }
