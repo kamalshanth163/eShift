302839ddbb70dee95dc4fe17787a5fad4c9bba48
kamal-maverick <kamalshanthdeveloper163@gmail.com>
2025-07-21 19:26:09 +0530

    Add ManageProductsForm

--- eShift/Forms/AdminForms/ManageProductsForm.cs
+++ eShift/Forms/AdminForms/ManageProductsForm.cs
@@ -1,16 +1,22 @@
 ﻿using eShift.Models;
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace eShift.Forms.AdminForms
 {
     public partial class ManageProductsForm : Form
     {
+        private DataGridView dgvProducts;
+        private Button btnClose;
+        private Button btnAddProduct;
+        private Button btnDeleteProduct;
+        private DataGridViewTextBoxColumn Code;
+        private DataGridViewTextBoxColumn ProductName;
+        private DataGridViewTextBoxColumn Description;
+        private DataGridViewTextBoxColumn Fee;
+        private Label lblTitle;
+
         public ManageProductsForm()
         {
             InitializeComponent();
@@ -60,6 +66,115 @@ namespace eShift.Forms.AdminForms
         private void btnClose_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void InitializeComponent()
+        {
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnAddProduct = new System.Windows.Forms.Button();
+            this.btnDeleteProduct = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.Code = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProductName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fee = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Code,
+            this.ProductName,
+            this.Description,
+            this.Fee});
+            this.dgvProducts.Location = new System.Drawing.Point(119, 141);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.ReadOnly = true;
+            this.dgvProducts.Size = new System.Drawing.Size(718, 316);
+            this.dgvProducts.TabIndex = 53;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(771, 63);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 52;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnAddProduct
+            // 
+            this.btnAddProduct.Location = new System.Drawing.Point(505, 63);
+            this.btnAddProduct.Name = "btnAddProduct";
+            this.btnAddProduct.Size = new System.Drawing.Size(104, 23);
+            this.btnAddProduct.TabIndex = 51;
+            this.btnAddProduct.Text = "Add Product";
+            this.btnAddProduct.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteProduct
+            // 
+            this.btnDeleteProduct.Location = new System.Drawing.Point(634, 63);
+            this.btnDeleteProduct.Name = "btnDeleteProduct";
+            this.btnDeleteProduct.Size = new System.Drawing.Size(112, 23);
+            this.btnDeleteProduct.TabIndex = 50;
+            this.btnDeleteProduct.Text = "Delete Product";
+            this.btnDeleteProduct.UseVisualStyleBackColor = true;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(88, 54);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(173, 24);
+            this.lblTitle.TabIndex = 49;
+            this.lblTitle.Text = "Manage Products";
+            // 
+            // Code
+            // 
+            this.Code.HeaderText = "Code";
+            this.Code.Name = "Code";
+            this.Code.ReadOnly = true;
+            // 
+            // ProductName
+            // 
+            this.ProductName.HeaderText = "Product Name";
+            this.ProductName.Name = "ProductName";
+            this.ProductName.ReadOnly = true;
+            // 
+            // Description
+            // 
+            this.Description.HeaderText = "Description";
+            this.Description.Name = "Description";
+            this.Description.ReadOnly = true;
+            // 
+            // Fee
+            // 
+            this.Fee.HeaderText = "Fee";
+            this.Fee.Name = "Fee";
+            this.Fee.ReadOnly = true;
+            // 
+            // ManageProductsForm
+            // 
+            this.ClientSize = new System.Drawing.Size(934, 511);
+            this.Controls.Add(this.dgvProducts);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnAddProduct);
+            this.Controls.Add(this.btnDeleteProduct);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "ManageProductsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
     }
 }
